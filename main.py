0633d0a580be9e097898ad716908e3db99b828d9
Santiago Reyes Gomez <91472195+Saregom@users.noreply.github.com>
2023-07-10 12:58:09 -0500

    progreso clase

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Bienvenido a la app de ReFashioned.'
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()
+    

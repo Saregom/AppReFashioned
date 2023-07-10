9adfe90b40d0a947cd41c4b4cd7633c584c5209f
Saregom <santi26reyes@gmail.com>
2023-07-09 23:14:21 -0500

    put

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Bienvenido a la app de ReFashioned'
+    return 'Bienvenido a la app de ReFashioned.'
 
 if __name__ == '__main__':
     app.run() 

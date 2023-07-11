ac7bda575718d98720f54fcd21567c714f6a4559
Saregom <santi26reyes@gmail.com>
2023-07-11 18:01:56 -0500

    Home, portada, productos

--- main.py
+++ main.py
@@ -13,13 +13,23 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    
     return render_template('signin.html')
 
 @app.route('/home')
 def home():
-    return render_template('home.html', navbar = True)
+    return render_template('home.html')
+
+@app.route('/products')
+def products():
+    products = [{'Nombre':'Pantalon', 'Precio':50000}, 
+                {'Nombre':'Camisa', 'Precio':20000}]
+
+    return render_template('products.html', products = products)
 
 if __name__ == '__main__':
     app.run()
